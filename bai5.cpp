4d125c19e80b68566cf5b773160f08039a2f4233
KimiPrime <kingsan049@gmail.com>
2024-11-29 00:59:28 +0700

    Add files via upload

--- bai5.cpp
+++ bai5.cpp
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int i=0,j,sum=0;
-    int arr[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-    while(i<4){
-    	j=0;
-        while(j<4){
-        	if(i==0||i==3||j==0||j==3){
-        		sum+=arr[i][j];
-			}
-			j++;
+    int sum = 0;
+    int arr[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
+    };
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+        
+            if (i == 0 || i == 3 || j == 0 || j == 3) {
+                sum += arr[i][j];
+            }
         }
-        i++;
     }
-    printf("tong duong bien la %d",sum);
-	return 0;
+
+    printf("Tong duong bien la: %d\n", sum);
+    return 0;
 }
 
